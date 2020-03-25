fe62fb1cb71a35e10f1fc055c55c46ea43de8efb
bbedevian <brettbedevian@gmail.com>
2020-03-25 18:30:42 -0400

    working on genres by year

--- lib/movies.rb
+++ lib/movies.rb
@@ -1,3 +1,5 @@
+require_all 'lib'
+
 class Movie < ActiveRecord::Base
     has_many :genre_movies
     has_many :genres, through: :genre_movies
@@ -7,10 +9,24 @@ class Movie < ActiveRecord::Base
     # validates :title,  :released, :runtime, :rating, :budget, :box_office, presence: true 
 
     def release_year
-        self.release.year
+        self.released.year
     end
 
-    #validates :title,  :released, :runtime, :rating, :budget, :box_office, presence: true 
+    def self.get_movie_genres(movie_name)
+        movie_id = find_movie_id(movie_name)
+        genres = GenresMovie.all.where(movie_id: movie_id)
+
+    end
+    
+    def self.movies_by_year
+        mby={} #movies by year hash
+        Movie.all.each do |movie|
+            if !(mby[movie.release_year])
+                mby[movie.release_year]= {}
+            end
+        end 
+        mby
+    end
 
 end
 
