b13602737aebcc8b6f489297ef0a2fc8c29c7d58
github username <github email address>
2020-03-25 14:52:38 -0400

    genres_actors method

--- lib/genres.rb
+++ lib/genres.rb
@@ -3,4 +3,22 @@ class Genre < ActiveRecord::Base
     has_many :movies, through: :genre_movies
 
     validates :name, presence: true 
+
+    def self.get_genres_actors(genre_name)
+        actor_array = []
+       genre_id=  Genre.all.find_by(name: genre_name).id
+        movies = GenresMovie.all.where(genre_id: genre_id)
+        movies.each do |movie|
+            movie_id = movie.movie_id
+            actors = ActorMovie.all.where(movie_id: movie_id)
+            actors.map do |actor|
+                actor_list = Actor.all.find_by(id: actor.actor_id).name
+                actor_array << actor_list
+            end
+        end
+     actor_array
+    end
+
+
+
 end
--- lib/movies.rb
+++ lib/movies.rb
@@ -3,6 +3,6 @@ class Movie < ActiveRecord::Base
     has_many :genres, through: :genre_movies
     has_many :actor_movie
     has_many :actors, through: :actor_movie
-    validates :title,  :released, :runtime, :rating, :budget, :box_office, presence: true 
+    # validates :title,  :released, :runtime, :rating, :budget, :box_office, presence: true 
 end
 
