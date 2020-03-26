17c62fad24d973894ef07522a758104498fcffad
Asilver-jpg <asilver7493@gmail.com>
2020-03-26 13:32:01 -0400

    falsey commit

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,23 @@
 require_relative '../config/environment'
 
+require 'tty-prompt'
 
+
+@@prompt= TTY::Prompt.new
+def run_application 
+
+system("clear")
+menu
+end
+
+def menu
+    system "clear"
+
+@@prompt.select("What u wanna do?") do |menu|
+    menu.choice "run thing", -> {Actor.average_genre_participation_hash}
+end
+end
+
+run_application
 
 puts "HELLO WORLD"
