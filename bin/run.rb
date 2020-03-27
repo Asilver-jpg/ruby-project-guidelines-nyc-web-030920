5234c8f40de791225b1c99d26e3ac085b48996c4
Asilver-jpg <asilver7493@gmail.com>
2020-03-27 10:26:17 -0400

    not using

--- bin/run.rb
+++ bin/run.rb
@@ -2,7 +2,6 @@ require_relative '../config/environment'
 
 require 'tty-prompt'
 
-
 @@prompt= TTY::Prompt.new
 def run_application 
 
@@ -14,8 +13,13 @@ def menu
     system "clear"
 
 @@prompt.select("What u wanna do?") do |menu|
-    menu.choice "run thing", -> {Actor.average_genre_participation_hash}
+    menu.choice "run thing", -> { puts Actor.average_genre_participation_hash}
+    menu.choice "run thing 2"
 end
+end
+
+def use_method
+   
 end
 
 run_application
